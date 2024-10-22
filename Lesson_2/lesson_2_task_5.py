123117d3f4b7cf923023f0b8a7ae70ef0d39cd93
ra3gon <ra3gon@icloud.com>
2024-10-22 22:58:14 +0300

    sintax fix

--- Lesson_2/lesson_2_task_5.py
+++ Lesson_2/lesson_2_task_5.py
@@ -1,14 +1,15 @@
 def month_to_season(n):
-        if n >= 1 and n <= 2 or n == 12:
-            return("Winter")
-        elif n >= 3 and n <= 5:
-            return("Spring")
-        elif n >= 6 and n <= 8:
-            return("Summer")
-        elif n >= 9 and n <= 11:
-            return("Autumn")
-        else:
-            return("out of range")
+    if n >= 1 and n <= 2 or n == 12:
+        return ("Winter")
+    elif n >= 3 and n <= 5:
+        return ("Spring")
+    elif n >= 6 and n <= 8:
+        return ("Summer")
+    elif n >= 9 and n <= 11:
+        return ("Autumn")
+    else:
+        return ("out of range")
+
 
 n = int(input("Month num: "))
 result = month_to_season(n)
