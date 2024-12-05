4e09855b2aa1e1c16ab71d0a62f90645cafb04e3
Aleksey Razgonyaev <ra3gon@icloud.com>
2024-12-05 17:17:13 +0300

    Remove .aqaenv

--- encode.py
+++
@@ -1,6 +0,0 @@
-import chardet
-
-# Определим кодировку файла
-with open(r'D:\AQA\aqaenv\Lib\site-packages\IPython\core\tests\nonascii.py', 'rb') as f:
-    result = chardet.detect(f.read())
-    print(result)
